bdac2172b439f04a8e5af9e7d31ea91568d4df0b
Santi <sbellodev@gmail.com>
2023-03-03 13:26:44 +0100

    fix -  404 page
    Needed to move resource to bottom

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,14 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::prefix('shops')->group(function () {
-    Route::get('/', [ShopController::class, 'index'])->name('shops.index');
-    Route::get('/createview', [ShopController::class, 'create'])->name('shops.create');
-    Route::post('/create', [ShopController::class, 'store'])->name('shops.store');
-    Route::get('/delete/{id}', [ShopController::class, 'delete'])->name('shops.delete');
-});
+Route::get('shops/index', [ShopController::class, 'index'])->name('shops.index');
+Route::get('shops/create', [ShopController::class, 'create'])->name('shops.create');
+Route::get('shops/{id}/edit', [ShopController::class, 'edit'])->name('shops.edit');
+Route::get('shops/delete/{id}', [ShopController::class, 'delete'])->name('shops.delete');
+Route::get('shops/{id}/info', [ShopController::class, 'showShop'])->name('shops.showShop');
+Route::put('shops/{id}', [ShopController::class, 'update'])->name('shops.update');
 
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::resource('shops', ShopController::class);
-
-// 
